94b087b114ec0dce24f74db004c48ecf4f4040d3
TomD <tomas.diessner@gmail.com>
2026-02-04 22:57:38 +0100

    Minesweeper - obecný fajly

--- Minesweeper/Minesweeper/MainWindow.xaml.cs
+++ Minesweeper/Minesweeper/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using Minesweeper.ViewModel;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,8 @@ namespace Minesweeper
         public MainWindow()
         {
             InitializeComponent();
+            MainWindowViewModel vm = new MainWindowViewModel();
+            DataContext = vm;
         }
 
     }

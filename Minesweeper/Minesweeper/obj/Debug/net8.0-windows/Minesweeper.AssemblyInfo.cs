3d234cbe1ca6dbf9a7cdc251489f9ef1ee8b8505
TomD <tomas.diessner@gmail.com>
2026-02-08 20:12:32 +0100

    zkouska 3

--- Minesweeper/Minesweeper/obj/Debug/net8.0-windows/Minesweeper.AssemblyInfo.cs
+++ Minesweeper/Minesweeper/obj/Debug/net8.0-windows/Minesweeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35a6a1018c93566916c275abc465437bdaf60d7c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1652dfc8bd420338b69ea967b625a41327da8eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
